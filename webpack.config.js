4a7f1f23b7639f4130e91d5acf3bddfe697e04ab
cryptofuker <your-email@example.com>
2025-06-17 00:25:24 +0800

    Fix webpack configuration for GitHub Pages paths

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,8 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            publicPath: '/whackcat/'
         }),
         new CleanWebpackPlugin(),
         new CopyWebpackPlugin({
